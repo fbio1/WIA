2e66574ca457b8c8288559a927bbb8c375434beb
fbio_ <fbio_henrique15@hotmail.com>
2018-05-22 09:15:26 -0300

    Alteração no actionBar na MainActivity e att sdk

--- app/src/main/java/com/wia/activity/MainActivity.java
+++ app/src/main/java/com/wia/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -39,6 +40,9 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Lista de Locais");
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
